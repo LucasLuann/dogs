26c183d69646a025eadc97d6e5ef21ba34516e28
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-01-10 08:58:33 -0400

    Header e SVG Assets

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import Dogs from "../Assets/dogs.svg?react"
 
 const Header = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">login / Criar</Link>
+    <header className={`${styles.header}`}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" aria-label="Dogs - Home" className={`${styles.logo}`}>
+          <Dogs />
+        </Link>
+        <Link to="/login" className={`${styles.login}`}>login / Criar</Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
