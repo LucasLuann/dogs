0ec3a25535b1033d628398f83f71b6b609ce5b99
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-01-13 15:51:28 -0400

    Adicionando contexto de usuário e lógica de autenticação

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 import Dogs from "../Assets/dogs.svg?react"
+import { UserContext } from "../UserContext";
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
+
   return (
     <header className={`${styles.header}`}>
       <nav className={`${styles.nav} container`}>
         <Link to="/" aria-label="Dogs - Home" className={`${styles.logo}`}>
           <Dogs />
         </Link>
-        <Link to="/login" className={`${styles.login}`}>login / Criar</Link>
+        {data && data.email}
+        <Link to="/login" className={`${styles.login}`}>
+          {data ? data.username : "Login / Criar"}
+        </Link>
       </nav>
     </header>
   );
