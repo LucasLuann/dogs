b28790e9e3112c6bc5625d78b723d4307e401baa
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-01-27 13:11:52 -0400

    Fix: Renderização condicional do link no header. Se já existir a conta, navega para a rota de /conta, se não existir, vai pra tela de login.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
-import Dogs from "../Assets/dogs.svg?react"
+import Dogs from "../Assets/dogs.svg?react";
 import { UserContext } from "../UserContext";
 
 const Header = () => {
   const { data } = React.useContext(UserContext);
-
 
   return (
     <header className={`${styles.header}`}>
@@ -14,10 +13,15 @@ const Header = () => {
         <Link to="/" aria-label="Dogs - Home" className={`${styles.logo}`}>
           <Dogs />
         </Link>
-        {data && data.email}
-        <Link to="/login" className={`${styles.login}`}>
-          {data ? data.username : "Login / Criar"}
-        </Link>
+        {data ? (
+          <Link to="/conta" className={`${styles.login}`}>
+            {data.nome}
+          </Link>
+        ) : (
+          <Link to="/login" className={`${styles.login}`}>
+            {data ? data.username : "Login / Criar"}
+          </Link>
+        )}
       </nav>
     </header>
   );
